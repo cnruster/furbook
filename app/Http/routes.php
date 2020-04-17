9b23e350bb3bab688b6752a9fc1b2c45f710f9eb
cnruster <cyp820@qq.com>
2020-04-17 17:37:27 +0800

    writing the first routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('cats/{id}', function($id) {
+    return sprintf('Cat #%s', $id);
+});
+
+Route::get('cats', function() {
+    return 'All cats';
+});
+
+Route::get('about', function() {
+    return view('about')->with('number_of_cats', 9000);
+});
