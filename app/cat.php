39c810b0cd33bfab7e8c1a51ffd3fa447c65a3a9
cnruster <cyp820@qq.com>
2020-04-19 12:24:42 +0800

    created the breeds table

---
+++ app/cat.php
@@ -0,0 +1,20 @@
+<?php namespace Furbook;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cat extends Model {
+
+    public $timestamps = false;
+    
+    protected $fillable = [
+              'name',
+              'date_of_birth',
+              'breed_id',
+          ];
+    
+    public function breed()
+    {
+        return $this->belongsTo('Furbook\Breed');
+    }
+
+}
