f9e36136ad459df94a6d6124a8b7bd9b65f76065
cnruster <cyp820@qq.com>
2020-04-23 01:10:33 +0800

    adding dependency laravelcollective/html

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -41,6 +41,7 @@ return array(
     'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
     'Doctrine\\Common\\Inflector\\' => array($vendorDir . '/doctrine/inflector/lib/Doctrine/Common/Inflector'),
     'Cron\\' => array($vendorDir . '/mtdowling/cron-expression/src/Cron'),
+    'Collective\\Html\\' => array($vendorDir . '/laravelcollective/html/src'),
     'ClassPreloader\\' => array($vendorDir . '/classpreloader/classpreloader/src'),
     '' => array($vendorDir . '/nesbot/carbon/src'),
 );
